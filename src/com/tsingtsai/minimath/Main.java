8a1e4dc6a3d1229e1045e6e8ebf52cc222475bf8
strongwillow <clear.tsai@gmail.com>
2017-01-12 15:52:37 +0100

    init

---
+++ src/com/tsingtsai/minimath/Main.java
@@ -0,0 +1,11 @@
+package com.tsingtsai.minimath;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        System.out.println("SDFSDF");
+
+    }
+}
